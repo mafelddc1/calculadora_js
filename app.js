a8e8240703d368495059cf1946ef12cc8aa83370
Maria Rivas <maria.rivas@sansano.usm.cl>
2019-10-09 18:21:58 -0300

    mas funciones

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 var calculadora = {
     sumar: function sumarNumeros(n1, n2) {
-        int n;
-        n=n1+n2;
-        return n1-n2;
+        return n1+n2;
        //
     },
 
     restar: function restarNumeros(n1, n2) {
-       //
-    },
+       return n1-n2;
+       
+           },
     
     multiplicar: function multiplicarNumeros(n1, n2) {
         //
