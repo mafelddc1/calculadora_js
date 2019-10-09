f22b4c5eece768b2dd8f3cc06dd2f7c0120e9f16
Telejuan <juan.sanchez.13@sansano.usm.cl>
2019-10-09 18:19:09 -0300

    jeje

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 var calculadora = {
-  
+    sumar: function sumarNumeros(n1, n2) {
+       //
+    },
+
+    restar: function restarNumeros(n1, n2) {
+       //
+    },
+    
+    multiplicar: function multiplicarNumeros(n1, n2) {
+        //
+    },
+
+    dividir: function dividirNumeros(n1, n2) {
+        //
+    }
 
 };
